8b2b0d5aaefe1978f6369fa0c371a8166aa78335
Owais siddiqui <83368515+owaiss259@users.noreply.github.com>
2026-03-06 01:03:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,6 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-
-class Settings:
-    API_KEY = os.getenv("API_KEY")
-    JENKINS_URL = os.getenv("JENKINS_URL")
-    JENKINS_USER = os.getenv("JENKINS_USER")
-    JENKINS_TOKEN = os.getenv("JENKINS_TOKEN")
-
-
-settings = Settings()
+JENKINS_URL = os.getenv("JENKINS_URL")
+JENKINS_USER = os.getenv("JENKINS_USER")
+JENKINS_API_TOKEN = os.getenv("JENKINS_API_TOKEN")
+API_KEY = os.getenv("API_KEY")
