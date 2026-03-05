710903050ed1b226b7436a8d60a80c0822daa1ce
Owais siddiqui <83368515+owaiss259@users.noreply.github.com>
2026-03-06 00:32:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 class Settings:
     API_KEY = os.getenv("API_KEY")
-
     JENKINS_URL = os.getenv("JENKINS_URL")
     JENKINS_USER = os.getenv("JENKINS_USER")
     JENKINS_TOKEN = os.getenv("JENKINS_TOKEN")
